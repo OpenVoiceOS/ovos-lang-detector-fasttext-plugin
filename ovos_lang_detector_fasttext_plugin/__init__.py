af83fbb795be80b7e9fac33ccf33da8d44009187
PocketMiner82 <73847326+PocketMiner82@users.noreply.github.com>
2024-04-04 16:03:16 +0200

    add another missing import

--- ovos_lang_detector_fasttext_plugin/__init__.py
+++ ovos_lang_detector_fasttext_plugin/__init__.py
@@ -2,6 +2,7 @@ from ovos_plugin_manager.templates.language import LanguageDetector
 from langcodes import standardize_tag
 from ovos_plugin_manager.templates.language import LanguageDetector
 from huggingface_hub import hf_hub_download
+import fasttext
 
 
 class FastTextLangDetectPlugin(LanguageDetector):
