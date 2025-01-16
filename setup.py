d67c1bcab95874e532feda824aa8d67f435ebe73
miro <jarbasai@mailfence.com>
2025-01-16 03:08:17 +0000

    fix: semver

--- setup.py
+++ setup.py
@@ -4,6 +4,29 @@ from setuptools import setup
 BASEDIR = os.path.abspath(os.path.dirname(__file__))
 
 
+def get_version():
+    """ Find the version of the package"""
+    version_file = os.path.join(BASEDIR, 'ovos_lang_detector_fasttext_plugin', 'version.py')
+    major, minor, build, alpha = (None, None, None, None)
+    with open(version_file) as f:
+        for line in f:
+            if 'VERSION_MAJOR' in line:
+                major = line.split('=')[1].strip()
+            elif 'VERSION_MINOR' in line:
+                minor = line.split('=')[1].strip()
+            elif 'VERSION_BUILD' in line:
+                build = line.split('=')[1].strip()
+            elif 'VERSION_ALPHA' in line:
+                alpha = line.split('=')[1].strip()
+
+            if ((major and minor and build and alpha) or
+                    '# END_VERSION_BLOCK' in line):
+                break
+    version = f"{major}.{minor}.{build}"
+    if alpha and int(alpha) > 0:
+        version += f"a{alpha}"
+    return version
+
 def required(requirements_file):
     """ Read requirements file and remove comments and empty lines. """
     with open(os.path.join(BASEDIR, requirements_file), 'r') as f:
@@ -22,7 +45,7 @@ PLUGIN_ENTRY_POINT = (
 
 setup(
     name='ovos-lang-detector-fasttext-plugin',
-    version='0.0.0a2',
+    version=get_version(),
     packages=['ovos_lang_detector_fasttext_plugin'],
     url='https://github.com/OpenVoiceOS/ovos-lang-detector-fasttext-plugin',
     license='apache-2',
